fd1d265d2944d85e9775d316929123a3e91af656
AtleSkaanes <118199811+AtleSkaanes@users.noreply.github.com>
2022-12-09 23:09:32 +0100

    IsThisThingOn?

---
+++ GameJam 09-12-22 Sne/Assets/Hewwo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hewwo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string coolEmoticon = ":J";
+        Debug.Log(coolEmoticon);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
