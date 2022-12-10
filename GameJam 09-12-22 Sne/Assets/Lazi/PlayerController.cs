279fe72c2d8a1226bc3af0988b69563940dccaae
LuckyEag1e <lazichkin@gmail.com>
2022-12-10 16:42:28 +0100

    Rotating sprite Rev 2

--- GameJam 09-12-22 Sne/Assets/Lazi/PlayerController.cs
+++ GameJam 09-12-22 Sne/Assets/Lazi/PlayerController.cs
@@ -6,28 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
-<<<<<<< Updated upstream
-=======
+
     Animator anim;
->>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-<<<<<<< Updated upstream
-=======
         anim = GetComponent<Animator>();
->>>>>>> Stashed changes
     }
 
     private void Update()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 relMousePos = new Vector2(mousePos.x-transform.position.x,mousePos.y-transform.position.y);
+        Vector2 relMousePos = new(mousePos.x-transform.position.x,mousePos.y-transform.position.y);
         float hypotenuse = Mathf.Sqrt(Mathf.Pow(relMousePos.x, 2)+Mathf.Pow(relMousePos.y, 2));
         float relMouseRad = Mathf.Asin(relMousePos.y/hypotenuse);
+        Debug.Log(mousePos);
         float relMouseDeg = relMouseRad*Mathf.Rad2Deg;
+
         if (mousePos.x <0 && mousePos.y <0)
         {
             relMouseDeg = 180+(relMouseDeg * -1);
@@ -39,68 +36,47 @@ public class PlayerController : MonoBehaviour
             relMouseDeg = 360-(relMouseDeg*-1);
         }
 
+        Debug.Log(relMouseDeg);
+
         if (relMouseDeg >=0 && relMouseDeg <45)
         {
-            Debug.Log("Q0.5");
-<<<<<<< Updated upstream
-        }else if (relMouseDeg >= 45 && relMouseDeg < 90)
-        {
-            Debug.Log("Q1");
-=======
+            //Debug.Log("Q0.5");
             anim.SetInteger("Dir", 1);
-        }else if (relMouseDeg >= 45 && relMouseDeg < 90)
+        }
+        else if (relMouseDeg >= 45 && relMouseDeg < 90)
         {
-            Debug.Log("Q1");
+            //Debug.Log("Q1");
             anim.SetInteger("Dir", 4);
->>>>>>> Stashed changes
         }
         else if (relMouseDeg >= 90 && relMouseDeg < 135)
         {
-            Debug.Log("Q1.5");
-<<<<<<< Updated upstream
-=======
+            //Debug.Log("Q1.5");
             anim.SetInteger("Dir", 4);
->>>>>>> Stashed changes
         }
         else if (relMouseDeg >= 135 && relMouseDeg < 180)
         {
-            Debug.Log("Q2");
-<<<<<<< Updated upstream
-        }else if (relMouseDeg >= 180 && relMouseDeg < 225)
-        {
-            Debug.Log("Q2.5");
-=======
+            //Debug.Log("Q2");
             anim.SetInteger("Dir", 3);
         }
         else if (relMouseDeg >= 180 && relMouseDeg < 225)
         {
-            Debug.Log("Q2.5");
+            //Debug.Log("Q2.5");
             anim.SetInteger("Dir", 3);
->>>>>>> Stashed changes
         }
         else if (relMouseDeg >= 225 && relMouseDeg < 270)
         {
-            Debug.Log("Q3");
-<<<<<<< Updated upstream
-=======
+            //Debug.Log("Q3");
             anim.SetInteger("Dir", 2);
->>>>>>> Stashed changes
         }
         else if (relMouseDeg >= 270 && relMouseDeg < 315)
         {
-            Debug.Log("Q3.5");
-<<<<<<< Updated upstream
-=======
+            //Debug.Log("Q3.5");
             anim.SetInteger("Dir", 2);
->>>>>>> Stashed changes
         }
         else if (relMouseDeg >= 315 && relMouseDeg < 360)
         {
-            Debug.Log("Q4");
-<<<<<<< Updated upstream
-=======
+            //Debug.Log("Q4");
             anim.SetInteger("Dir", 1);
->>>>>>> Stashed changes
         }
 
     }
