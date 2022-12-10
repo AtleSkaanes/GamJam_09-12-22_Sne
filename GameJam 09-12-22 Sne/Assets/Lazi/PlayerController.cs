ecd38a3c4d7f3fd8ed3483426a9b3f6ffaa0360b
LuckyEag1e <lazichkin@gmail.com>
2022-12-10 16:01:00 +0100

    Rotating sprite

--- GameJam 09-12-22 Sne/Assets/Lazi/PlayerController.cs
+++ GameJam 09-12-22 Sne/Assets/Lazi/PlayerController.cs
@@ -6,11 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
+<<<<<<< Updated upstream
+=======
+    Animator anim;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+<<<<<<< Updated upstream
+=======
+        anim = GetComponent<Animator>();
+>>>>>>> Stashed changes
     }
 
     private void Update()
@@ -34,32 +42,65 @@ public class PlayerController : MonoBehaviour
         if (relMouseDeg >=0 && relMouseDeg <45)
         {
             Debug.Log("Q0.5");
+<<<<<<< Updated upstream
         }else if (relMouseDeg >= 45 && relMouseDeg < 90)
         {
             Debug.Log("Q1");
+=======
+            anim.SetInteger("Dir", 1);
+        }else if (relMouseDeg >= 45 && relMouseDeg < 90)
+        {
+            Debug.Log("Q1");
+            anim.SetInteger("Dir", 4);
+>>>>>>> Stashed changes
         }
         else if (relMouseDeg >= 90 && relMouseDeg < 135)
         {
             Debug.Log("Q1.5");
+<<<<<<< Updated upstream
+=======
+            anim.SetInteger("Dir", 4);
+>>>>>>> Stashed changes
         }
         else if (relMouseDeg >= 135 && relMouseDeg < 180)
         {
             Debug.Log("Q2");
+<<<<<<< Updated upstream
         }else if (relMouseDeg >= 180 && relMouseDeg < 225)
         {
             Debug.Log("Q2.5");
+=======
+            anim.SetInteger("Dir", 3);
+        }
+        else if (relMouseDeg >= 180 && relMouseDeg < 225)
+        {
+            Debug.Log("Q2.5");
+            anim.SetInteger("Dir", 3);
+>>>>>>> Stashed changes
         }
         else if (relMouseDeg >= 225 && relMouseDeg < 270)
         {
             Debug.Log("Q3");
+<<<<<<< Updated upstream
+=======
+            anim.SetInteger("Dir", 2);
+>>>>>>> Stashed changes
         }
         else if (relMouseDeg >= 270 && relMouseDeg < 315)
         {
             Debug.Log("Q3.5");
+<<<<<<< Updated upstream
+=======
+            anim.SetInteger("Dir", 2);
+>>>>>>> Stashed changes
         }
         else if (relMouseDeg >= 315 && relMouseDeg < 360)
         {
             Debug.Log("Q4");
+<<<<<<< Updated upstream
+=======
+            anim.SetInteger("Dir", 1);
+>>>>>>> Stashed changes
         }
 
     }
