e777cfb8f3bc41a6daff3ed37add6731d95cbf60
KneeCapStealer <87864377+KneeCapStealer@users.noreply.github.com>
2022-12-10 17:00:13 +0100

    Added bullets and made ranged enemies shoot

---
+++ GameJam 09-12-22 Sne/Assets/Christoffer/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Transform playerTrans;
+
+    public float bulletSpeed = 2f;
+
+    Vector3 direction;
+
+    float time = 0f;
+
+    private void Awake ()
+    {
+        playerTrans = GameObject.FindWithTag("Player").transform;
+        direction = (playerTrans.position - this.transform.position).normalized;
+    }
+
+    void Update()
+    {
+        this.transform.position += direction * bulletSpeed * Time.deltaTime;
+
+        time += Time.deltaTime;
+        if (time > 10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //do dmg to player
+            Destroy(this.gameObject);
+        }
+    }
+}
